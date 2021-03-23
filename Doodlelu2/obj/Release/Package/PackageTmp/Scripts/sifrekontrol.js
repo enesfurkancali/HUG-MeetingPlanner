dcc2bc45eb667a9cbdb8fec1c159e53418c3ca33
enesfurkancali <67002809+enesfurkancali@users.noreply.github.com>
2021-03-23 11:39:13 +0300

    .

---
+++ Doodlelu2/obj/Release/Package/PackageTmp/Scripts/sifrekontrol.js
@@ -0,0 +1,13 @@
+﻿var password = document.getElementById("sifre")
+    , confirm_password = document.getElementById("sifrekontrol");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Girdiğiniz şifreler eşleşmiyor. Lütfen tekrar deneyiniz.");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
